8576484ba0e4b0d9cd0a376c1ff19ad29c53a091
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2015-12-25 07:52:51 +0200

    Adds email uniqueness tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,4 +23,16 @@ RSpec.describe User, type: :model do
       expect(user).to_not be_valid
     end
   end
+  context 'unique emails' do
+    let(:first) {FactoryGirl.create(:user)}
+    let(:second) {FactoryGirl.build(:user)}
+    it 'should disallow duplicate emails' do
+      second.email = first.email
+      expect(second).to_not be_valid
+    end
+    it 'should perform case insensitive check' do
+      second.email = first.email.upcase
+      expect(second).to_not be_valid
+    end
+  end
 end
